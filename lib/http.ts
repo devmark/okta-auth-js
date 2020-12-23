daf310d1af6e91f8517fe485dc3bb8b1ff864bc6
Mark <mark@emcoo.com>
2020-12-23 16:48:57 +0800

    updated

--- lib/http.ts
+++ lib/http.ts
@@ -104,6 +104,8 @@ function httpRequest(sdk: OktaAuth, options: RequestOptions): Promise<any> {
 
       err = new AuthApiError(serverErr, resp);
 
+      storage.set('AUTH_API_ERROR', err, new Date(Date.now() + 10000).toDateString(), sdk.options.cookies);
+
       if (err.errorCode === 'E0000011') {
         storage.delete(STATE_TOKEN_KEY_NAME);
       }
